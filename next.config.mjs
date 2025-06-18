d8d26645b6f9e29eb82c8fca40ea4cacae7318a0
RezwanKhan47 <143204798+RezwanKhan47@users.noreply.github.com>
2025-06-19 02:02:16 +0600

    commit
    19/6/25

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'images.pexels.com'],
+    domains: ["images.pexels.com"],
     remotePatterns: [
       {
         protocol: 'https',
